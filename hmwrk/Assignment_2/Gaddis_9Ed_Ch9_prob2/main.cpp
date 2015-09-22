04bd915b4b14b58e0595f4c246d3b696bee06d5a
Hao Huynh <hah.ptit@gmail.com>
2015-09-22 14:42:29 -0700

    'added_sort_vector_med'

--- hmwrk/Assignment_2/Gaddis_9Ed_Ch9_prob2/main.cpp
+++ hmwrk/Assignment_2/Gaddis_9Ed_Ch9_prob2/main.cpp
@@ -14,6 +14,7 @@
 #include <algorithm>
 #include <vector>
 
+
 using namespace std;
 //Global Constants
 
@@ -26,9 +27,9 @@ float getMed(int *,int); //get the median of array - use pointer notation
 //Execution begins here 
 int main(int argc, char** argv) {
     //Declare variables
-    const int SIZE = 20; //Size of the array
+    srand(time(NULL));   //Random Number seed to initialize array
+    int SIZE = (rand()%30)+1; //size of the array will be random
     int arr[SIZE];      //array where list will be stored
-    srand(time(NULL));//Random Number seed to initialize array
     float med;          //variable to store median value
     
     //Initialize array with random values 1-100
@@ -36,35 +37,38 @@ int main(int argc, char** argv) {
         arr[i] = (rand()%100)+1;
         cout<<arr[i]<<endl;
     }
-    
-    for (int i = 0; i<SIZE;i++){
-        arr[i] = (rand()%100)+1;
-        cout<<arr[i]<<endl;
-    }
-    
+    cout<<endl<<endl;
     //Pass to the get median function to determine median
     med = getMed(arr,SIZE);
     
     //Output the median 
-    
+    cout<<"The array had "<<SIZE<<" numbers"<<endl;
+    cout<<"The median of the list is "<<med<<endl;
     
     //Exit stage right!
     return 0;
 }
 
-float getMed(int *arr,int SIZE){
+float getMed(int *arr,int size){
     //Declare Variables
-    int med; //Median of the array
-    vector<int> temp(arr,arr+20); //create a temp vector to store the array
+    float med; //Median of the array
+    vector<int> temp; //create a temp vector to store the array
+    temp.assign(arr, arr + size); //puts all the arr data into the vector
     
-    //Sort the vector in ascending order
-    sort(temp.begin(),temp.end());
+    //Sort the vector in ascending order using sort function
+    sort(temp.begin(),temp.end()); 
     
+    //Output the new sorted list to verify sort
+    for(int i =0;i<size;i++){
+        cout<<temp[i]<<endl;
+        }
     
-    
-    
-    //Determine if the median is even or odd
-    
+    //Determine if median is even or odd - then determine median number
+     if (size  % 2 == 0){ //determines if even
+      med = (temp[size / 2 - 1] + temp[size / 2]) / 2;
+    }else { //Odd number of values = size / 2;
+      med = temp[size / 2];
+    }
     
     //Return the median back to the main function
     return med; 
