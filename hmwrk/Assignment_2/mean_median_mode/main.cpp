c8280d4fd0abe126f90f8dd862ee8f357a3073e6
amulbham <amul.bham@gmail.com>
2015-10-02 17:36:46 -0700

    started mean median and mode problem

---
+++ hmwrk/Assignment_2/mean_median_mode/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Amul Bham
+ * Purpose: Calculate the Mean,
+ * Median, mode, etc. of
+ * a repeating array
+ * Created on October 2, 2015, 5:28 PM
+ */
+//System Libraries
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+//Global Constants
+
+//User Libraries
+
+//Function Prototypes
+
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    
+    
+    return 0;
+}
+
