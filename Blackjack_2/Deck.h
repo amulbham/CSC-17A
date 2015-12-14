dc9e42e8d124abbb29a9b4bb7429cbb1badb6c75
Amul Bham <amul.bham@gmail.com>
2015-12-14 00:48:18 -0800

    error, forgot to save, this is the commit with the line comments for main and deck function

--- Blackjack_2/Deck.h
+++ Blackjack_2/Deck.h
@@ -1,7 +1,22 @@
 /* 
  * File:   Deck.h
  * Author: Amul.bham
- *
+ * Purpose: The deck classes was responsible for all the dealings of 
+ * the deck of cards used in the game. The idea was to wrap
+ * all the complexity of creating a deck of cards, I really tried to make 
+ * the class a individual meaning it can be applied to really any card game,
+ * the drawCard function does it all, wraps up all the dealings of the deck in 
+ * one function, prints out the cards, and returns a card value
+ * The Class creates a vector of values 2-53 and then shuffles that vector
+ *  - The card is drawn starting from value zero and then the variable is incremented
+ * after each card draw
+ * - Once the card number hits a certain limit, the deck is re shuffled automatically, 
+ * displaying a message to the players so they know. 
+ * - The name of each card is printed out as a string, and the value of the card
+ * is returned back to the the caller for what ever purpose.
+ *  -Ex: for blackjack, the card value is returned and added to the players
+ * overall card total.
+ *  
  * Created on December 8, 2015, 7:37 PM
  */
 
@@ -29,9 +44,19 @@ public:
     Deck(const Deck& orig);
     virtual ~Deck();
 private:
+    /*Vector used to store values 2-53 to represent cards in a deck
+     allowed for use of random_shuffle function and easier manipulation*/
     vector<int> deck;
+    /*Card int used track the current card # of the card being drawn, it is
+     incremented after each draw for the next card
+     total stored the card value of card that was drawn, as the card # had no
+     relevance to the actual value of the card*/
     int card,total,y;
+    /*Face and Value used to store the string representations of the card that was drawn,
+     they are then sent to the display cards function for output, kept separate from
+     the value of the card itself as face cards were not represented as numbers*/
     string face,value;
+    //Static const used to represent the number of cards in a deck, ensured that this value did not change
     static const int tCards = 53;
     
 
--- Blackjack_2/main.cpp
+++ Blackjack_2/main.cpp
@@ -33,58 +33,107 @@ void displayGreeting(); //function for greeting player
 //Execution begins here
 int main(int argc, char** argv) {
     //Declare Variables
+    
+    /*Both of these variables were used as outside switch variables
+     to trigger what happens to the other classes and functions, 
+     if the player is playing with others, a vector is created, 
+     agn is used to control the game loop, which starts at the beginning of 
+     each hand and ends when agn turns to 'no'*/
      int p;
      char agn;
 
-     
+     //Display a greeting to the user
     cout<<"                 Casino Bham's BlackJack"<<endl<<endl;
 
-    //Determine how many players are going to play
+    /*Determine how many players are going to play, from here
+     a vector is created with p players of the player class
+     ex: p = 3; 3 instances of player class are created and place in a vector*/
     do{
      cout<<"How many players would like to play? (Up to 4 total)"<<endl;
      cin>>p;
-     
+     //Ensure the amount of players is not over 4 as it can get hectic on one screen
      if (p<1 || p>4) {cout<<"Invalid players!"<<endl;}
     }while(p<1 || p>4);
-     /*Determine for each player if they are returning or a new player
-      If they are returning, read out the name and balance in memory of the player
-      if they are new, get the player name, and player buy in amount*/
+     /*Create a new instance of the blackJack class, this class control
+      all of the game flow of blackJack game for the user, meaning the player
+      and deck classes are totally separate and stand alone, in the future more
+      game can be added here by simply creating a new class for the game and
+      adding a menu here for each game*/
      blackJack game(p);
      
+     
+     /*Game Loop starts here -> from here is where each new hand starts,
+      the players have the choice to leave after each hand, if they do want to 
+      play again, their card totals are reset, however their names and 
+      binary file positions are maintained as that was done in the 
+      creation of the blackJack object*/
      do{
      game.disBord();
      /*Get the bets for the hand from each player, keep track of the bet amount
      , starting balance, and if the player has overdrawn and needs to buy in again*/
      game.setBets();
      game.disBord();
-     //Deal the first two cards to the player
+     /*The first cards are dealt to each player,the drawnCard function of the 
+      * card class is called, a card is printed out, and its value is
+      * passed back and added to the players overall card total for 
+      * the hand, if the player has any significant hands (blackjacks, busts, etc) 
+      * they are displayed after they are encountered.
+      each players hand has been displayed*/
      game.dealCards();
      
      game.disBord();
  
      
-     //Deal the rest of the player hands
+     /*At this point, the players must decide if they want to hit or stay
+      depending on their current card value, if a player decides to hit or stay,
+      each player is given the turn and can continue till they decide to stay,
+      go over 21 or hit a 21, at which point it begins the next players turn
+      Status is tracked after each players hand is finished, meaning
+      if they lose they are marked as a loss, if they hit a 21 they are marked
+      * as a possible win,if they decide to stay under 21 they are marked for compare */
     game.hitORstay();
     game.disBord();
     
-    //Deal the rest of the dealers hand
+    /*After the hit stay portion of the hand, assuming that at least 1 of the players
+     has not 'busted', the rest of the dealer hand is dealt till the dealer
+     hit a value of 17 of greater All the cards were displayed and valued
+     using the drawn card function in the deck class*/
     game.dealerHand();
     game.disBord();
     
-    //Calculate the Winners of the hands based on card totals and status of each player
+    /*Using the status of each player, and the dealer, which was tracked
+     after each card draw, a final comparison is held between each player
+     and the dealer card value, So for example if the dealer had a 21, 
+     any player who is not marked with a status of '21' automatically
+     * loses, and the players who do have a 21 are marked as a tie. After this
+     * the balance of each player is recalculated based on the status of 
+     * their hand, tie = no money loss, loss = loss of bet etc.  */
     game.checkWinner();
     
-    //Output the results of the hand to the user 
+    /*The results of the hand are displayed in a overall report at the end
+     of each hand and a recalculated balance is printed out for each 
+     player, from here they can decide to play another hand or end the game*/
     game.showResults();
     game.disBord();
-    
+    /*The players can now decide, after seeing their results, if they want to 
+     play again*/
     cout<<"Would you like to play another hand? Y/N"<<endl; cin.ignore();
     cin.get(agn); 
-     if (tolower(agn) == 'y') game.newHand();
+    /*If the players do want to play a new hand, then the game loops back to the start
+     of the doWhile loop, all the card values and totals are reset for each player*/
+     if (tolower(agn) == 'y') game.newHand();// <- Resets each players hand
      }while(tolower(agn) == 'y');
-
+     
+     /*If the players decide they are finished playing, each players information
+     including their name and balance are stored in a file, this allows each player
+     to have their own separate account in the game and can maintain
+     their own balance in the game. If they already have a spot on the binary
+     file, their spot is overwritten using random access file entry, and overriding
+     the spot on file that represents their binary number, if they are a new player
+     their information is appended to the file in a new spot and a binary number
+     representing their spot on the file is assigned to the new player*/
      game.writeInfo();
      
-     
+     //Exit Stage Right!
  return 0;    
 }
